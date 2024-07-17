9534b87eb3b4667cfdd1603e2afb63c5421d6f28
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-07-17 09:31:51 -0500

    fixed: resolved errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
-import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  vite: {
+    resolve: {
+      alias: {
+        '@': new URL('./src', import.meta.url).pathname,
+      },
+    },
+  },
+})
