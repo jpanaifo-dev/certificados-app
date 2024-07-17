2c21e0c4269986ea8eb5b4524c66e63ce435f31e
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-07-17 12:43:43 -0500

    fixed: resolved errors

--- src/components/core/Table.tsx
+++ src/components/core/Table.tsx
@@ -59,7 +59,7 @@ export const Table = (props: Props) => {
                 </div>
               </td>
               <td className="px-6 py-4 whitespace-nowrap">
-                <div className="text-sm text-gray-900">{certificate.curso}</div>
+                <div className="text-sm text-gray-900 uppercase">{certificate.curso}</div>
               </td>
               <td className="px-6 py-4 whitespace-nowrap">
                 <span className="text-sm text-gray-900">
